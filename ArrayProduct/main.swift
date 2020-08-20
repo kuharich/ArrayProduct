2a39532cc8decfb91de796e43e1578e13052eb09
MARK KUHARICH <kuharich@hotmail.com>
2020-08-20 11:12:03 -0700

    Initial Commit

---
+++ ArrayProduct/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ArrayProduct
+//
+//  Created by Mark Kuharich on 8/20/20.
+//  Copyright © 2020 Mark Kuharich. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
